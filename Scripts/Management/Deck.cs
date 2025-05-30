2ac1635f65168c64df7376f9e33a2b72bc3731fc
mtaraf <marcostaraf16@hotmail.com>
2025-05-30 15:15:00 -0400

    Added to UI and started Deck/GameManager scripts

---
+++ Scripts/Management/Deck.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    [SerializeField] public int maxDeckSize = 20;
+    [SerializeField] public int deckSize = 0;
+    [SerializeField] public List<CardModelSO> deck;
+    [SerializeField] public GameObject cardPrefab;
+
+    [SerializeField] private GameObject deckSizeText;
+
+    [SerializeField] private GameObject cardSlotContainer;
+
+    private List<GameObject> cardSlots = new List<GameObject> { };
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Get card slots
+        foreach (Transform child in cardSlotContainer.transform)
+        {
+            Debug.Log(child.gameObject.name);
+            cardSlots.Add(child.gameObject);
+        }
+
+        deckSizeText.GetComponent<TextMeshProUGUI>().text = deckSize.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Deck clicked");
+    }
+
+    void OnMouseEnter()
+    {
+        Debug.Log("Deck Hovered");
+    }
+
+    void AddCardToDeck(CardModelSO card)
+    {
+        if (deckSize != maxDeckSize)
+        {
+            deck.Add(card);
+        }
+    }
+
+    void drawCard(CardModelSO card)
+    {
+        foreach (GameObject slot in cardSlots)
+        {
+            if (slot.transform.childCount == 0)
+            {
+                // Add Card to this slot
+
+                break;
+            }
+        }
+    }
+}
