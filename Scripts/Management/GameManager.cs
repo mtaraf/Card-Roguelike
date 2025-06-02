8b4348e54d585017257d3a8d6f3b281d23365d8c
mtaraf <marcostaraf16@hotmail.com>
2025-06-02 12:32:46 -0400

    More Menu Changes

--- Scripts/Management/GameManager.cs
+++ Scripts/Management/GameManager.cs
@@ -4,6 +4,15 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    // Menu
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private GameObject loadGameMenu;
+
+
+    // Game States
+
+
     public void Awake()
     {
         if (instance != null)
@@ -14,5 +23,36 @@ public class GameManager : MonoBehaviour
 
         instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    // Game Functions
+
+    // Save Functions
+
+    // Menu functions
+    public void playButtonClicked()
+    {
+        mainMenu.gameObject.SetActive(false);
+        loadGameMenu.gameObject.SetActive(true);
+        Debug.Log("Play clicked!");
+    }
+
+    public void openSettings()
+    {
+        Debug.Log("Open Settings");
+        mainMenu.gameObject.SetActive(false);
+        optionsMenu.gameObject.SetActive(true);
+    }
+
+    public void returnToMainMenu()
+    {
+        loadGameMenu.gameObject.SetActive(false);
+        optionsMenu.gameObject.SetActive(false);
+        mainMenu.gameObject.SetActive(true);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Quit Game");
     }
 }
